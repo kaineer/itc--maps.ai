fb5d9ce7ed6ac031d7b0e79c035e49a47e569b72
Sergey Kluchkovsky <kaineer@gmail.com>
2025-12-08 17:17:32 +0500

    Add pm2 config

---
+++ stages/pm2.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "serve-api-and-models",
+      cwd: "./serve_buildings",
+      script: "npm run dev",
+    },
+    {
+      name: "frontend",
+      cwd: "./display_buildings",
+      script: "npm run dev",
+    },
+  ]
+};
